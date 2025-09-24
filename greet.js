8daaee9ec8fcfff62d1f0a449975edbeaf1a2c1e
Mustafa Lowna <mustafalowna321@gmail.com>
2025-09-24 13:08:47 +0530

    Add greet.js, oddeven-t.js, smart-tables.js, And ternary.js For Greeting Users, Determining Odd/Even Numbers, Printing Multiplication Tables, And Calculating Ticket Costs Based On Age

---
+++ greet.js
@@ -0,0 +1,10 @@
+function greet() {
+  console.log("Hi");
+}
+greet();
+function greetA(userName) {
+  console.log("Hi " + userName + "! How Are You?");
+}
+greetA("Mustafa");
+greetA("Ahmed");
+greetA("Arham");
---
+++ ternary.js
@@ -0,0 +1,5 @@
+let age = 25;
+let ticketCost = 0;
+
+ticketCost = age >= 18 ? 500 : 250;
+console.log("Ticket Cost = Rs " + ticketCost);
