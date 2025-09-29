a6c5105725a2fcf704edd940069676b5d4a9dece
Mustafa Lowna <mustafalowna321@gmail.com>
2025-09-29 14:36:31 +0530

    Add arrow-functions.js For Demonstrating Arrow Function Syntax With Examples

---
+++ Assignments/arrow-functions.js
@@ -0,0 +1,14 @@
+const greet = (name) => "Hello, " + name + "!";
+console.log(greet("John"));
+
+const getArea = (radius) => 3.142 * radius * radius;
+console.log(getArea(10));
+
+const findMax = (a, b) => (a > b ? a : b);
+console.log(findMax(10, 20));
+
+const isEven = (num) => num % 2 === 0;
+console.log(isEven(7));
+
+const welcome = () => "Welcome to JavaScript";
+console.log(welcome());
