bd038b1237562fd9e0f9f3a56b95c6eb715de31c
Mustafa-Lowna <mustafalowna321@gmail.com>
2025-10-19 19:44:23 +0530

    Add Test Folder & Add Test-01.js File

---
+++ Tests/Test-01.js
@@ -0,0 +1,5 @@
+let numbers = [4, 7, 12, 15, 9];
+let modifiedNumbers = numbers.map((n) => (n % 2 == 0 ? n * 2 : n + 1));
+console.log(modifiedNumbers);
+
+// console.log([4, 7, 12, 15, 9].map((n) => (n % 2 == 0 ? n * 2 : n + 1)));
